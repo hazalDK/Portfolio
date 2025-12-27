2f10c788b1dee4062205cabfb17a445a548b0908
Hazal Deniz Kara <hazalkara1212@gmail.com>
2025-12-27 19:12:44 +0000

    nav bar progress

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,14 +1,17 @@
+"use client";
+
 import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
 } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const navigation = [
-  { name: "Home", href: "/", current: true },
-  { name: "About", href: "/about", current: false },
+  { name: "Resume", href: "/about", current: false },
+  { name: "Skills", href: "/skills current: false" },
   { name: "Projects", href: "/projects", current: false },
-  { name: "Contact", href: "/contact", current: false },
 ];
 
 function classNames(...classes: (string | boolean | undefined)[]) {
@@ -16,29 +19,38 @@ function classNames(...classes: (string | boolean | undefined)[]) {
 }
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans bg-emerald-200">
+    <div className="min-h-screen font-sans bg-green-200 pt-4">
       <Disclosure
         as="nav"
-        className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+        className="relative bg-white mx-4 rounded-xl after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-green-200"
       >
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              {/* Mobile menu button*/}
-              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-                <span className="absolute -inset-0.5" />
+            {/* Mobile menu button */}
+            <div className="absolute inset-y-0 right-0 flex items-center justify-end sm:hidden">
+              <DisclosureButton
+                onClick={() => setOpen(!open)}
+                className="group relative inline-flex items-right justify-right rounded-md p-2 text-black focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500"
+              >
                 <span className="sr-only">Open main menu</span>
+                {open ? (
+                  <XMarkIcon
+                    className="h-6 w-6 text-black"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  <Bars3Icon
+                    className="h-6 w-6 text-black"
+                    aria-hidden="true"
+                  />
+                )}
               </DisclosureButton>
             </div>
+
+            {/* Desktop menu */}
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex shrink-0 items-center">
-                <img
-                  alt="Your Company"
-                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                  className="h-8 w-auto"
-                />
-              </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
                   {navigation.map((item) => (
@@ -48,9 +60,9 @@ export default function Home() {
                       aria-current={item.current ? "page" : undefined}
                       className={classNames(
                         item.current
-                          ? "bg-gray-950/50 text-white"
-                          : "text-gray-300 hover:bg-white/5 hover:text-white",
-                        "rounded-md px-3 py-2 text-sm font-medium"
+                          ? "bg-gray-950/50 text-black"
+                          : "text-black hover:bg-green-200",
+                        "rounded-md px-3 py-2 text-sm font-medium",
                       )}
                     >
                       {item.name}
@@ -59,10 +71,10 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
           </div>
         </div>
 
+        {/* Mobile menu */}
         <DisclosurePanel className="sm:hidden">
           <div className="space-y-1 px-2 pt-2 pb-3">
             {navigation.map((item) => (
@@ -73,9 +85,9 @@ export default function Home() {
                 aria-current={item.current ? "page" : undefined}
                 className={classNames(
                   item.current
-                    ? "bg-gray-950/50 text-white"
-                    : "text-gray-300 hover:bg-white/5 hover:text-white",
-                  "block rounded-md px-3 py-2 text-base font-medium"
+                    ? "bg-gray-950/50 text-black"
+                    : "text-black hover:bg-green-200",
+                  "block rounded-md px-3 py-2 text-base font-medium",
                 )}
               >
                 {item.name}
